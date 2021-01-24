968b2b3861357095d9fdc2ebe0ae733c5ee08e0a
Aliaksandr Senkevich <puskin1914@gmail.com>
2021-01-25 00:23:21 +0300

    [fix]: change api url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from "./App";
 import "./index.css";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: "https://my-graphql-apollo-server.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
